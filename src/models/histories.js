c371b0e4629b00a0a847751dc3abad468a9828c0
Nabil Hardiansa <nhardiansa@gmail.com>
2022-01-30 21:31:29 +0800

    membuat model addHistory mereturn promise

--- src/models/histories.js
+++ src/models/histories.js
@@ -1,10 +1,26 @@
 const db = require('../helpers/db');
-const historiesTable = 'histories';
+const {
+  historiesTable,
+} = require('../helpers/constant');
 
 module.exports = {
   getHistories: (keys, cb) => {
     db.query(`SELECT ?? FROM ${historiesTable}`, [keys], (err, results) => {
+      if (err) throw err;
       cb(results);
+    });
+  },
+
+  addHistory: (data) => {
+    return new Promise((resolve, reject) => {
+      db.query(`INSERT INTO ${historiesTable} SET ?`, data, (err, results) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+        } else {
+          resolve(results);
+        }
+      });
     });
   },
 };
