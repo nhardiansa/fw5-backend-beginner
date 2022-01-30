0cc3a123934a7789dd46fb0c99ffc763816979be
Nabil Hardiansa <nhardiansa@gmail.com>
2022-01-30 21:32:07 +0800

    membuat route addHistory

--- src/routes/histories.js
+++ src/routes/histories.js
@@ -3,8 +3,10 @@ const histories = new express.Router();
 
 const {
   getHistories,
+  addHistory,
 } = require('../controllers/histories');
 
 histories.get('/', getHistories);
+histories.post('/', addHistory);
 
 module.exports = histories;
