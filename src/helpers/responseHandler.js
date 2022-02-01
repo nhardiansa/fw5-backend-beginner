89e8a05ebffecdb414e3d4c070fbeb6a714103e5
Nabil Hardiansa <nhardiansa@gmail.com>
2022-02-01 18:26:57 +0800

    menambah pageCreator link

--- src/helpers/responseHandler.js
+++ src/helpers/responseHandler.js
@@ -21,3 +21,33 @@ exports.returningSuccess = (res, status, message, data, pageInfo = null) => {
     result: data
   });
 };
+
+exports.pageCreator = (url, values) => {
+  const keys = [];
+  let next = url;
+  let prev = url;
+
+  for (const key in values) {
+    keys.push(key);
+  }
+
+  keys.forEach((el, idx) => {
+    if (values[el]) {
+      if (el === 'page') {
+        next += el + '=' + (values[el] + 1) + '&';
+        prev += el + '=' + (values[el] - 1) + '&';
+      } else if (idx < (keys.length - 1)) {
+        next += el + '=' + values[el] + '&';
+        prev += el + '=' + values[el] + '&';
+      } else {
+        next += el + '=' + values[el];
+        prev += el + '=' + values[el];
+      }
+    }
+  });
+
+  return ({
+    next,
+    prev
+  });
+};
