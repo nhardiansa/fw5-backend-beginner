4850f1b576a57cf436590c48bb433f850827e3b3
Nabil Hardiansa <nhardiansa@gmail.com>
2022-02-01 14:26:36 +0800

    menambah kondisi ketika pageinfo dimasukan

--- src/helpers/responseHandler.js
+++ src/helpers/responseHandler.js
@@ -5,7 +5,16 @@ exports.returningError = (res, status, message) => {
   });
 };
 
-exports.returningSuccess = (res, status, message, data) => {
+exports.returningSuccess = (res, status, message, data, pageInfo = null) => {
+  if (pageInfo) {
+    return res.status(status).json({
+      success: true,
+      message: message,
+      result: data,
+      pageInfo
+    });
+  }
+
   return res.status(status).json({
     success: true,
     message: message,
