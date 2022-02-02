cebe0aaa9e00760ba331bcefc94d8077f8e49ac3
Nabil Hardiansa <nhardiansa@gmail.com>
2022-02-02 21:02:21 +0800

    membuat queryCreator untuk where dan like

---
+++ src/helpers/queryCreator.js
@@ -0,0 +1,36 @@
+exports.whereLikeCreator = (data) => {
+  if (!(typeof data === 'object')) {
+    return '';
+  }
+
+  if (Array.isArray(data)) {
+    return '';
+  }
+
+  const keys = [];
+  let str = Object.keys(data).length ? 'WHERE ' : '';
+
+  if (data) {
+    for (const key in data) {
+      if (data[key]) {
+        keys.push(key);
+      }
+    }
+
+    keys.forEach((el, idx) => {
+      if (data[el]) {
+        if (idx < keys.length - 1) {
+          str += `${el} LIKE '${data[el]}%' AND `;
+        } else {
+          str += `${el} LIKE '${data[el]}%'`;
+        }
+      }
+    });
+  }
+
+  if (!(str.length > 6)) {
+    str = '';
+  }
+
+  return str;
+};
