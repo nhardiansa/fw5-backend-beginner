6adc94d647387ce11c495ab44ca2c37700d45c9d
Nabil Hardiansa <nhardiansa@gmail.com>
2022-02-08 16:56:41 +0800

    feat: add env config

--- src/helpers/db.js
+++ src/helpers/db.js
@@ -1,10 +1,18 @@
 const mysql = require('mysql');
+require('dotenv').config();
+
+const {
+  DB_HOST,
+  DB_USER,
+  DB_PASSWORD,
+  DB_NAME
+} = process.env;
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'vehicle_rent'
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME
 });
 
 connection.connect();
