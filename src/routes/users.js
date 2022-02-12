509259ac649d59d88b251f0ee19532e2ce107513
Nabil Hardiansa <nhardiansa@gmail.com>
2022-02-12 21:47:44 +0800

    feat: add email verification route

--- src/routes/users.js
+++ src/routes/users.js
@@ -12,21 +12,23 @@ const {
   listUsers,
   registerUser,
   loginUser,
-  forgotPassword,
-  resetPassword
+  sentConfirmationCode,
+  resetPassword,
+  emailVerifcation
 } = require('../controllers/users');
 
 const auth = require('../middlewares/auth');
 
 users.get('/', auth.verifyAdmin, listUsers);
 users.get('/profile/:id', getUser);
-users.get('/:id', getUser);
+users.get('/:id', auth.verifyUser, getUser);
 
 users.post('/', auth.verifyAdmin, uploadMiddleware('image'), addUser);
 users.post('/register', registerUser);
 users.post('/auth/login', loginUser);
-users.post('/forgotPassword', forgotPassword);
+users.post('/forgotPassword', sentConfirmationCode);
 users.post('/resetPassword', resetPassword);
+users.post('/emailVerifcation', emailVerifcation);
 
 users.patch('/:id', auth.verifyUser, uploadMiddleware('image'), updateUser);
 
