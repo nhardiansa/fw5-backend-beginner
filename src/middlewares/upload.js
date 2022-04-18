85229c065c3e63916515cf84ea8008e3fc209293
Nabil Hardiansa <nhardiansa@gmail.com>
2022-04-18 11:40:17 +0800

    feat: use cloudinary as storage with functionality

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -1,16 +1,50 @@
+const {
+  CloudinaryStorage
+} = require('multer-storage-cloudinary');
+const cloudinary = require('cloudinary').v2;
 const multer = require('multer');
 const {
   returningError
 } = require('../helpers/responseHandler');
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/');
-  },
-  filename: function (req, file, cb) {
-    const extension = file.originalname.split('.').pop();
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    cb(null, file.fieldname + '-' + uniqueSuffix + '.' + extension);
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, 'uploads/');
+//   },
+//   filename: function (req, file, cb) {
+//     const extension = file.originalname.split('.').pop();
+//     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+//     cb(null, file.fieldname + '-' + uniqueSuffix + '.' + extension);
+//   }
+// });
+
+const {
+  CLOUD_NAME,
+  CLOUD_API_KEY,
+  CLOUD_API_SECRET
+} = process.env;
+
+cloudinary.config({
+  cloud_name: CLOUD_NAME,
+  api_key: CLOUD_API_KEY,
+  api_secret: CLOUD_API_SECRET
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: (req) => {
+      const {
+        baseUrl
+      } = req;
+      return `otorent/${baseUrl}`;
+    },
+    format: (req, file) => 'jpg',
+    public_id: (req, file) => {
+      // const extension = file.originalname.split('.').pop();
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+      return file.fieldname + '-' + uniqueSuffix;
+    }
   }
 });
 
